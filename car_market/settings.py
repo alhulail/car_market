e9a760d9e2bd6dd1ca742796ba3cb27566ea6b88
Hadeel AlHulail <hadeel@Hadeels-MacBook-Pro.local>
2018-08-02 14:11:44 +0300

    prepare for deployment

--- car_market/settings.py
+++ car_market/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mmj9k6e^e=si_+-lz-505sj(!)-!vly6zy3+xb(89^ldu&wcb9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['159.65.142.91']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'car_market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-s-1vcpu-1gb-sgp1-01',
+        'USER': 'root',
+        'PASSWORD': '0cfb6f4f6d71c2b5924a77dda3',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -120,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- cars/models.py
+++ cars/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Car(models.Model):
 	make = models.CharField(max_length=120)
 	model = models.CharField(max_length=120)
 	year = models.IntegerField()
+	car_img = models.ImageField(upload_to='car_img', null=True)
 
 	def __str__(self):
-		return "{} {} - {}".format(self.make, self.model, self.year)
+		return "{} {} - {}".format(self.make, self.model, self.year, self.car_img)
 
 	def get_absolute_url(self):
 		return reverse('car-detail', kwargs={'car_id':self.id})
